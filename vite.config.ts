ae49916ac74052ee9951d04e4cbf9d22785006d4
liushuxue <shuxue.liu@jgdt.com>
2025-04-19 11:07:44 +0800

    vite 配置文件目录修改

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
 
-import { createVitePlugins, createbuildConfig, createCssConfig, createProxy } from "./config";
+import { createVitePlugins, createbuildConfig, createCssConfig, createProxy } from "./vite";
 
 // https://vite.dev/config/
 export default defineConfig(({ command, mode }) => {
